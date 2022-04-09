8053ff93ca20c9f1ee1533d2b5c6a7de8f3ad950
Pedro Henrique Cursino Cabral <97694134+WorenoKing@users.noreply.github.com>
2022-04-08 23:59:14 -0300

    initial

--- index.php
+++ index.php
@@ -13,20 +13,25 @@
 
 </head>
 <body>
-    
-
-
-
-
-<!--Foto, nome, status, e icones-->
-    <div class="initial-box">
-        <div class="col-lg-4">
-            <img src=" " alt="..." class="bd-placeholder-img" width="150" height="150">
-    
-            <h2 class="corletra">Matias Hamburgaria</h2>
-            <p><a class="btn btn-outline-warning" href="#">Aberto</a></p>
+ <header id="initial" >
+     <!--Menu inicial-->   
+    <div class="container col-lg-12">
+            <div class="menu">
+                <h3>MyLunch</h3>
+                    <input type="submit" class="botao">
             </div>
     </div>
+     
+     <!--Foto, nome, status, e icones-->
+        <div class="initial-box">
+            <div class="col-lg-4">
+                <img src=" " alt="..." class="bd-placeholder-img" width="150" height="150">
+     
+                <h2 class="corletra">Matias Hamburgaria</h2>
+                <p><a class="btn btn-outline-warning" href="#">Aberto</a></p>
+                </div>
+        </div>
+ </header>
 
 </body>
 </html>
