4d6e6e58fd23719e54dfd56f26a03e67b9a6293a
fernandoiezzi <102105297+fernandoiezzi@users.noreply.github.com>
2022-04-08 23:32:40 -0300

    cabeçalho
    botão cabeçalho

--- index.php
+++ index.php
@@ -4,33 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--style interno-->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!--style do bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>MyLunch</title>
-
 </head>
 <body>
     
-
-
-
-
-<!--Foto, nome, status, e icones-->
-    <div class="initial-box">
-        <div class="col-lg-4">
-            <img src=" " alt="..." class="bd-placeholder-img" width="150" height="150">
-    
-            <h2 class="corletra">Matias Hamburgaria</h2>
-            <p><a class="btn btn-outline-warning" href="#">Aberto</a></p>
-            </div>
-    </div>
-
 </body>
 </html>
-<!--script do bootstrap-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
