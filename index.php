004610ca56d30d3273fd9ded4a558f53c7744965
fernandoiezzi <102105297+fernandoiezzi@users.noreply.github.com>
2022-04-18 23:26:14 -0300

    Inclusão de rodapé - Front parceiro
    Incluindo rodapé - Front Parceiro

--- index.php
+++ index.php
@@ -6,19 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
     <title>Mylunch</title>
+    <link rel="icon" type="image.png" href="img/marca.png" />
 </head>
 <body>
 <!--Menu Superior-->
 <header id="meunu_superior">
         <div class="mylunch_name">
-                <h1><a style="color:#fa9f42" href="#">MY LANCH</a></h1>
+                <h1><a style="color:#fa9f42" href="#">MyLunch</a></h1>
         </div>
         <div class="cart-button">
             <a href="#">
             <svg xmlns="http://www.w3.org/2000/svg" class="carrinho" width="25" height="30" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg></a>
-            <a href=""><button type="button" action="#" class="menu-botao">LOGIN</button></a>
+            <button type="button" action="#" class="menu-botao" href="facebook.com">LOGIN</button>
         </div>          
 </header>
 <!--Fim Menu Superior-->
@@ -26,7 +27,7 @@
 
 <div class="info-proprietario">
     <div class="part-1">
-        <img class="logo-proprietario" width="250px" heidht="250" src="img/logotipo.png">
+        <img class="logo-proprietario" width="250px" heidht="250" src="img/marca.png">
     </div>
     <div class="part-2">
         <p class="nome-proprietario"><strong>FAUSTINO ARTSANAL</strong></p>
@@ -37,19 +38,17 @@
 
 </div>
 
-<div class="sticky">
-    <div class="menu-2">
-        <nav>
-            <ul class="menu-ul">
-                <li><input class="loopa" type="search" width="100px" height="50px" placeholder="Buscar"></li>
-                <li><a href="#">PROMOÇÕES DO DIA</a></li>
-                <li><a href="#">O SUPER LANCHE</a></li>
-                <li><a href="#">HAMBURGUERES</a></li>
-                <li><a href="#">BEBIDAS</a></li>
-                <li><a href="#">EXEMPLO</a></li>
-            </ul>
-        </nav>
-    </div>
+<div>
+    <nav>
+        <ul class="menu-ul">
+            <li><input class="loopa" type="search" width="100px" height="50px" placeholder="Buscar"></li>
+            <li><a class="ul-list" href="#">PROMOÇÕES DO DIA</a></li>
+            <li><a class="ul-list" href="#">O SUPER LANCHE</a></li>
+            <li><a class="ul-list" href="#">HAMBURGUERES</a></li>
+            <li><a class="ul-list" href="#">BEBIDAS</a></li>
+            <li><a class="ul-list" href="#">EXEMPLO</a></li>
+        </ul>
+    </nav>
 </div>
 
 
@@ -64,7 +63,11 @@
            <p style="font-size: 25px;">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua</p>
            <h2 style="font-size: 25px">R$:15,90</h2>
             </div>
-           <img src="img/burguer.png" width="170px" >
+            <div class="icon-plus">
+            <a href="#" target="_blank">
+                <img src="img/iconplus.jpg" width="80px" height="80px">
+            </a>
+           </div>
         </div>
         
         <div class="box2">
@@ -74,25 +77,35 @@
            <p style="font-size: 25px;">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua</p>
            <h2 style="font-size: 25px">R$:15,90</h2>
             </div>
-           <img src="img/burguer.png" width="170px" >
+            <div class="icon-plus">
+            <a href="#" target="_blank">
+            <img src="img/iconplus.jpg" width="80px" height="80px" >
+            </a>
+           </div>
         </div>
       
-        <div class="box3">terceiro</div>
-       
-        <div class="box4">quarto</div>
-</div>
-
-<footer>
-    <div id="rodape">
-        <div class="description">
-            <a href=""><strong>www.mylunch.com.br</strong></a>
-            <br>
+        <div class="box3">
+            terceiro
+            <div class="icon-plus">
+            <a href="#" target="_blank">
+            <img src="img/iconplus.jpg" width="80px" height="80px"></a>
+            </div>
         </div>
-        <div class="topo">
-            <a href="#"><input type="button" class="btn" value="Topo"></a>
+       
+        <div class="box4">
+            quarto
+            <div class="icon-plus">
+            <a href="#" target="_blank">
+            <img src="img/iconplus.jpg" width="80px" height="80px"></a>
+            </div>
         </div>
     </div>
-</footer>
+    
+    <div class="footer">
+        <h1>www.mylunch.com.br</h1>
+    </div>
+
+
     
 </body>
 </html>
